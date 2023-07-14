205fbd6ce2aba34add17ade512df4414c9c5bbbc
DilshadMr360 <mohomeddilshad123@gmail.com>
2023-07-14 09:53:22 +0530

    Changes

--- app/components/ApplicationLists.js
+++ app/components/ApplicationLists.js
@@ -64,74 +64,76 @@ const ApplicationLists = ({ data }) => {
           />
           {/* Rendering chassis , build date , and Odometer */}
           <View style={{ paddingLeft: 10 }}>
-            <Text style={{ left: 25, bottom: 5, color: '#000', fontWeight: '700' }}>{item.make}</Text>
+            <Text style={{ bottom: 5, color: '#000', fontWeight: '700' }}>{item.make}</Text>
             <View style={{ flexDirection: "row" }}>
               <View>
-                <Image
-                  source={require("../assets/chassis.png")}
-                  style={[
-                    styles.dashboardicon,
-                    {
-                      width: 16,
-                      height: 16,
-                      tintColor: "#000",
-                      top: 4,
-                      right: 10,
-                    },
-                  ]}
-                />
-                <Image
-                  source={require("../assets/built.png")}
-                  style={[
-                    styles.dashboardicon,
-                    {
-                      width: 16,
-                      height: 16,
-                      tintColor: "#000",
-                      top: 24,
-                      right: 10,
-                    },
-                  ]}
-                />
-                <Image
-                  source={require("../assets/odo.png")}
-                  style={[
-                    styles.dashboardicon,
-                    {
-                      width: 16,
-                      height: 16,
-                      tintColor: "#000",
-                      top: 45,
-                      right: 10,
-
-                    },
-                  ]}
-                />
-              </View>
-              <View>
-                <View>
+                <View style={{ flexDirection: 'row' }}>
+                  <Image
+                    source={require("../assets/chassis.png")}
+                    style={[
+                      styles.dashboardicon,
+                      {
+                        width: 16,
+                        height: 16,
+                        tintColor: "#000",
+                        top: 4,
+                        right: 10,
+                      },
+                    ]}
+                  />
                   <Text style={{ minHeight: 35 }}>Chassis:</Text>
-                  <Text style={{ width: 130, fontSize: 13, bottom: 15 }}>{item.chassis_no}</Text>
+                  <Text style={{ left: 20, width: 130, fontSize: 13 }}>{item.chassis_no}</Text>
+                </View>
+                <View style={{ flexDirection: "row" }}>
+                  <Image
+                    source={require("../assets/built.png")}
+                    style={[
+                      styles.dashboardicon,
+                      {
+                        width: 16,
+                        height: 16,
+                        tintColor: "#000",
+                        right: 10,
+                        bottom: 12,
+                      },
+                    ]}
+                  />
                   <Text style={{ bottom: 15 }}>Build Date:</Text>
-                  <Text style={{ width: 130, fontSize: 13, bottom: 15 }}>{`${item.build_month}/${item.build_year}`}</Text>
+                  <Text style={{ fontSize: 13, bottom: 15, left: 8, }}>{`${item.build_month}/${item.build_year}`}</Text>
+                </View>
+
+                <View style={{ flexDirection: 'row' }}>
+                  <Image
+                    source={require("../assets/odo.png")}
+                    style={[
+                      styles.dashboardicon,
+                      {
+                        width: 16,
+                        height: 16,
+                        tintColor: "#000",
+                        right: 10,
+                        bottom: 10
+                      },
+                    ]}
+                  />
                   <Text style={{ bottom: 12 }}>ODO:</Text>
-                  <Text style={{ width: 130, fontSize: 13, bottom: 15 }}>{item.odo_meter}</Text>
+                  <Text style={{ fontSize: 13, bottom: 12, left: 45, }}>{item.odo_meter}</Text>
+                </View>
+
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                  {/* <ApplicationFunctionsGradientButton text="View" onPress={() => navigation.navigate("ViewApplications", { applicationId: item.id })} /> */}
+                  <ApplicationFunctionsGradientButton text="Edit" onPress={() => navigation.navigate("EditApplications", { applicationId: item.id })} />
+                  <ApplicationFunctionsGradientButton text="Download" />
+                </View>
+                <View>
                 </View>
               </View>
-              {/* <View style={{ right: 50, bottom: 26 }}>
-                <Text style={{ top: 67, fontSize: 13, }}>{`${item.build_month}/${item.build_year}`}</Text>
-                <Text style={{ top: 70, fontSize: 13, }}>{item.odo_meter}</Text>
-              </View> */}
             </View>
           </View>
         </View>
 
         {/* View, Edit, Download Buttons */}
-        <View style={{ marginTop: 10, }}>
-          <ApplicationFunctionsGradientButton text="View" onPress={() => navigation.navigate("ViewApplications", { applicationId: item.id })} />
-          <ApplicationFunctionsGradientButton text="Edit" onPress={() => navigation.navigate("EditApplications", { applicationId: item.id })} />
-          <ApplicationFunctionsGradientButton text="Download" />
-        </View>
+
 
       </View>
     );
@@ -155,7 +157,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     width: "96%",
     paddingHorizontal: 10,
-    paddingVertical: 20,
+    paddingVertical: 10,
     justifyContent: "space-between",
     marginBottom: 10,
     top: 20,
